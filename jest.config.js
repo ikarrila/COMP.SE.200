74b4d44205a521ef8c2c8aca04a731b0d3a8bac9
ikarrila <mjunposti@gmail.com>
2024-11-03 13:03:27 +0200

    Parallel tests have running IDs

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 // jest.config.js
 export default {
   collectCoverage: true,
-  coverageDirectory: "coverage",
+  coverageDirectory: `coverage-${process.env.TEST_NUMBER}`, // Unique coverage folder for each parallel job
   coverageReporters: ["lcov", "text"]
 };
