612fd6109eea352df4d6aa133d462d724eda80b7
ikarrila <mjunposti@gmail.com>
2024-11-03 13:18:26 +0200

    Coveralls fixed

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,9 @@
 // jest.config.js
 export default {
   collectCoverage: true,
-  coverageDirectory: `coverage-${process.env.TEST_NUMBER}`, // Unique coverage folder for each parallel job
-  coverageReporters: ["lcov", "text"]
+  coverageDirectory: `coverage-${process.env.TEST_NUMBER}`,
+  coverageReporters: ["lcov", "text"],
+  transform: {
+    "^.+\\.js$": "babel-jest"
+  }
 };
