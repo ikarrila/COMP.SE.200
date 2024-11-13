ee795b00030561077d078f5ba51f1d88462a6216
Mehis <vikkek@gmail.com>
2024-11-13 15:33:35 +0200

    Restricted which tests are part of coverage

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,18 @@ export default {
   collectCoverage: true,
   coverageDirectory: `coverage-${process.env.TEST_NUMBER}`,
   coverageReporters: ["lcov", "text"],
+  collectCoverageFrom: [
+    "src/**/filter.js",
+    "src/**/isEmpty.js",
+    "src/**/chunk.js",
+    "src/**/eq.js",
+    "src/**/get.js",
+    "src/**/add.js",
+    "src/**/isDate.js",
+    "src/**/toNumber.js",
+    "src/**/reduce.js",
+    "src/**/isLength.js",
+  ],
   transform: {
     "^.+\\.js$": "babel-jest"
   },
