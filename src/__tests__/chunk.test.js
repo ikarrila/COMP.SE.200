cac00007251d898e901f3d9445e267469fdc482a
ikarrila <mjunposti@gmail.com>
2024-11-16 18:00:26 +0200

    Chunk test added

--- src/__tests__/chunk.test.js
+++ src/__tests__/chunk.test.js
@@ -1,47 +1,41 @@
-import chunk from '../chunk.js';
+import chunk from '../chunk';
 
-describe('chunk function', () => {
-  test('splits an array into groups of the specified size', () => {
-    const result = chunk(['a', 'b', 'c', 'd'], 2);
-    expect(result).toEqual([['a', 'b'], ['c', 'd']]);
-  });
-
-  test('handles arrays that cannot be split evenly', () => {
-    const result = chunk(['a', 'b', 'c', 'd'], 3);
-    expect(result).toEqual([['a', 'b', 'c'], ['d']]);
-  });
-
-  test('handles size greater than array length', () => {
-    const result = chunk(['a', 'b'], 5);
-    expect(result).toEqual([['a', 'b']]);
-  });
-
-  test('handles size of 0 or less', () => {
-    expect(chunk(['a', 'b', 'c'], 0)).toEqual([]);
-    expect(chunk(['a', 'b', 'c'], -1)).toEqual([]);
-  });
-
-  test('returns an empty array if the input array is null or undefined', () => {
-    expect(chunk(null, 2)).toEqual([]);
-    expect(chunk(undefined, 2)).toEqual([]);
-  });
-
-  test('returns an empty array for empty input arrays', () => {
-    expect(chunk([], 2)).toEqual([]);
-  });
-
-  test('handles the default size of 1', () => {
-    const result = chunk(['a', 'b', 'c']);
-    expect(result).toEqual([['a'], ['b'], ['c']]);
-  });
-
-  test('converts non-integer sizes to integers', () => {
-    const result = chunk(['a', 'b', 'c', 'd'], 2.5);
-    expect(result).toEqual([['a', 'b'], ['c', 'd']]);
-  });
-
-  test('handles size being a non-number (converts to 0)', () => {
-    expect(chunk(['a', 'b', 'c'], 'string')).toEqual([]);
-    expect(chunk(['a', 'b', 'c'], {})).toEqual([]);
-  });
+// Test ID UC3
+describe('chunk', () => {
+    test('splits array into chunks of size 2', () => {
+        expect(chunk(['a', 'b', 'c', 'd'], 2)).toEqual([['a', 'b'], ['c', 'd']]);
+    });
+    test('splits array into chunks of size 3', () => {
+        expect(chunk(['a', 'b', 'c', 'd'], 3)).toEqual([['a', 'b', 'c'], ['d']]);
+    });
+    test('splits array of size 1', () => {
+        expect(chunk(['a'], 1)).toEqual([['a']]);
+    });
+    test('splits array with size greater than array length', () => {
+        expect(chunk(['a', 'b', 'c'], 5)).toEqual([['a', 'b', 'c']]);
+    });
+    test('handles an empty array', () => {
+        expect(chunk([], 2)).toEqual([]);
+    });
+    test('handles null array input', () => {
+        expect(chunk(null, 2)).toEqual([]);
+    });
+    test('handles undefined array input', () => {
+        expect(chunk(undefined, 2)).toEqual([]);
+    });
+    test('handles size 0', () => {
+        expect(chunk(['a', 'b', 'c'], 0)).toEqual([]);
+    });
+    test('handles negative size', () => {
+        expect(chunk(['a', 'b', 'c'], -1)).toEqual([]);
+    });
+    test('handles size as a floating point number', () => {
+        expect(chunk(['a', 'b', 'c', 'd'], 2.5)).toEqual([['a', 'b'], ['c', 'd']]);
+    });
+    test('handles size as a string that can be converted to a number', () => {
+        expect(chunk(['a', 'b', 'c', 'd'], '2')).toEqual([['a', 'b'], ['c', 'd']]);
+    });
+    test('handles size as a string that cannot be converted to a number', () => {
+        expect(chunk(['a', 'b', 'c', 'd'], 'invalid')).toEqual([]);
+    });
 });
